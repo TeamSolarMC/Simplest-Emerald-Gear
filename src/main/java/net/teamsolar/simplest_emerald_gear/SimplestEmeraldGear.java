bdc3b9418fe2fdd49ffa489a8876ef91fd0d9d04
Denis Roman <lazarescodr@gmail.com>
2026-04-07 23:05:53 +0300

    Started porting to 1.21.11: added tools and armor

---
+++ src/main/java/net/teamsolar/simplest_emerald_gear/SimplestEmeraldGear.java
@@ -0,0 +1,68 @@
+package net.teamsolar.simplest_emerald_gear;
+
+import net.minecraft.world.item.CreativeModeTabs;
+import net.teamsolar.simplest_emerald_gear.item.ModItems;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(SimplestEmeraldGear.MODID)
+public class SimplestEmeraldGear {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "simplest_emerald_gear";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public SimplestEmeraldGear(IEventBus modEventBus, ModContainer modContainer) {
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        ModItems.ITEMS.register(modEventBus);
+
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(ModItems.EMERALD_SWORD);
+            event.accept(ModItems.EMERALD_AXE);
+            event.accept(ModItems.EMERALD_HELMET);
+            event.accept(ModItems.EMERALD_CHESTPLATE);
+            event.accept(ModItems.EMERALD_LEGGINGS);
+            event.accept(ModItems.EMERALD_BOOTS);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.EMERALD_SHOVEL);
+            event.accept(ModItems.EMERALD_PICKAXE);
+            event.accept(ModItems.EMERALD_AXE);
+            event.accept(ModItems.EMERALD_HOE);
+        }
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
